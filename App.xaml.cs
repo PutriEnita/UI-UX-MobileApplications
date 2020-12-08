8ccb601741438e0fd4f71cbfe4a7efa01ed12420
Putri Enita <46951339+PutriEnita@users.noreply.github.com>
2020-12-08 20:27:49 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Login
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            //Application.Current.Properties["IsLoggedIn"] = Boolean.TrueString;
+            //Application.Current.Properties["email"] = "iyonpriyono";
+
+            InitializeComponent();
+            bool isLoggedIn = Current.Properties.ContainsKey("IsLoggedIn") ? Convert.ToBoolean(Current.Properties["IsLoggedIn"]) : false;
+            
+
+            if (!isLoggedIn)
+            {
+                //Load if Not Logged In
+                MainPage = new Login.Pages.LoginPage();
+            }
+            else
+            {
+                //Load if Logged In
+                MainPage = new Login.Pages.Baru();
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts  
+        }
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps  
+        }
+        protected override void OnResume()
+        {
+            // Handle when your app resumes  
+        }
+    }
+}
